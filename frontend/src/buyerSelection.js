308833d61cc2649dbd294f185c67264249a4db3a
Apoorva30893 <apoorvag30@gmail.com>
2025-04-22 14:15:22 +0530

    Add files via upload

--- frontend/src/buyerSelection.js
+++ frontend/src/buyerSelection.js
@@ -1,34 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Modal, Select, Button } from 'antd';
-import QuotationForm from './quotationForm';
+//import QuotationForm from './quotationForm';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCompanies} from '../store/slices/companySlice'
 
 const { Option } = Select;
 
-const BuyerSelection = (props) => {
-    console.log("inside buyer selection",props);
-    const formtype = props.formtype
+const BuyerSelection = () => {
     const navigate = useNavigate();
     const [visible, setVisible] = useState(true);
-    const [isQuotationFormVisible, setIsQuotationFormVisible] = useState(false);
     const [selectedBuyer, setSelectedBuyer] = useState(null);
-    const [buyers, setBuyers] = useState([]); 
+    const dispatch = useDispatch();
+    const { loading, companies=[], error } = useSelector((state) => state.companyData);
 
-    useEffect(() => {
-       
-            fetchBuyerDetails();
-        }, []);
+  useEffect(() => {
+    dispatch(fetchCompanies());
+  }, [dispatch]);
+  console.log('companies:', companies);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
-     const fetchBuyerDetails = async () => {
-        try {
-            const response = await axios.get('http://localhost:5000/getAllCompanyies/'); // Replace with your actual API endpoint
-            setBuyers(response.data);
-            console.log(response.data);
-          } catch (error) {
-            console.error('Error fetching items:', error);
-          }
-        };
 
     const handleBuyerChange = (value) => {
         setSelectedBuyer(value);
@@ -36,26 +28,14 @@ const BuyerSelection = (props) => {
 
     const handleOk = () => {
         setVisible(false);
-        //setIsQuotationFormVisible(true);
-        if(formtype === 'SE'){
-            navigate('/enquiry', { state: { buyerName: selectedBuyer } });
-        }
-        if(formtype === 'SQ'){
-            navigate('/quotation', { state: { buyerName: selectedBuyer } });
-        }
-        if(formtype === 'RFQ'){
-            navigate('/requestForquotation', { state: { buyerName: selectedBuyer } });    
-        }
-        if(formtype === 'OC'){
-            navigate('/orderConfirmation', { state: { buyerName: selectedBuyer } });
-        }
+        navigate('/enquiry', { state: { buyerName: selectedBuyer } });
     };
 
     return (
         <>
             <Modal
                 title="Please Add/Select Buyer"
-                visible={visible}
+                open={visible}
                 onOk={handleOk}
                 onCancel={() => setVisible(false)}
                 footer={[
@@ -74,7 +54,7 @@ const BuyerSelection = (props) => {
                     onChange={handleBuyerChange}
                     style={{ width: '100%' }}
                 >
-                    {buyers.map(item => (
+                    {companies.map(item => (
                       <Option
                         key={item.buyername}
                         value={item.buyername}
@@ -83,9 +63,6 @@ const BuyerSelection = (props) => {
                         {item.buyername}
                       </Option>
                     ))}
-                    <Option value="SCHENCK PROCESS INDIA LIMITED">SCHENCK PROCESS INDIA LIMITED</Option>
-                    <Option value="Apoorva">Apoorva</Option>
-                    {/* Add more options here */}
                 </Select>
                 <Button type="link" style={{ marginTop: '10px' }}>
                     + Add New Company
